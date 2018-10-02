918fa3f4cebb8627e1c16a33ef766b462c6cfc26
Kerwin Chen <kchen37@stuy.edu>
2018-10-02 11:11:38 -0400

    as

---
+++ func.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct poke {char name[10]; int index;};
+
+/*
+int print(struct poke mon){
+  printf("the name of %c is %s of pokedex %d", mon, mon.name, mon.index);
+  return 0;
+  }*/
+//strcopy
+int main(){
+  struct poke a;
+  a.name[10] = 'cyndaquil';
+  a.index = 155;
+
+  printf("%s\n",a.name);
+  printf("%d\n",a.index);
+  return 0;
+}
