c3598a9c3134dcadef32b6971108f3db69e6920d
AntonDanylenko <adanylenko@stuy.edu>
2018-10-03 10:54:41 -0400

    added function that chooses random pokemon

--- func.c
+++ func.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "func.h"
 
 struct poke {char name[10]; int index;};
 
@@ -8,55 +10,8 @@ int print(struct poke mon){
   printf("the name of %c is %s of pokedex %d", mon, mon.name, mon.index);
   return 0;
   }*/
-//strcopy
-int main(){
-  struct poke a;
-  strcpy(a.name, "cyndaquil");
-  a.index = 155;
 
-  printf("%s\n",a.name);
-  printf("%d\n",a.index);
-
-  struct poke b;
-  strcpy(b.name, "snivy");
-  b.index = 495;
-
-  struct poke c;
-  strcpy(c.name, "servine");
-  c.index = 496;
-
-  struct poke d;
-  strcpy(d.name, "serperior");
-  d.index = 497;
-
-  struct poke e;
-  strcpy(e.name, "tepig");
-  e.index = 498;
-
-  struct poke f;
-  strcpy(f.name, "pignite");
-  f.index = 499;
-
-  struct poke g;
-  strcpy(g.name, "emboar");
-  g.index = 500;
-
-  struct poke h;
-  strcpy(h.name, "oshawott");
-  h.index = 501;
-
-  struct poke i;
-  strcpy(i.name, "dewott");
-  i.index = 502;
-
-  struct poke j;
-  strcpy(j.name, "samurott");
-  j.index = 503;
-
-  struct poke pokedex[10] = {a,b,c,d,e,f,g,h,i,j};
-
-  for(int x=0; x<10; x++){
-    printf("%d: %s, %d\n", x, pokedex[x].name, pokedex[x].index);
-  }
-  return 0;
+struct poke choosePoke(struct poke array[]){
+  srand(time(0));
+  return array[rand()%9];
 }
